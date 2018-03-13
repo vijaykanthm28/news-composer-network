f1254b08d7edde81fd9b6f01ad6cc41807ce1af8
vijaykanthm28 <mvijaykanth@megam.io>
2018-03-13 12:41:48 +0530

    build file change

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 npm install 
 
 # Deploys the BNA file into running Fabric blockchain network.
-composer network deploy -a dist/news-network.bna -p hlfv1 -i PeerAdmin -s randomString
+#composer network deploy -a dist/news-network.bna -p hlfv1 -i PeerAdmin -s randomString
 
 # Run the following command to verify by noting the success response
-composer network ping -n news-network -p hlfv1 -i admin -s adminpw
+# composer network ping -n news-network -p hlfv1 -i admin -s adminpw
